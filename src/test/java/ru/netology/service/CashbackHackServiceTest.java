c8f844c83785382e50e09b4e77ecef3f80dcaba9
LuNTIK969 <nasa.969@gmail.com>
2023-03-10 17:38:06 +0300

    TestNG, test added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,46 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService cash = new CashbackHackService();
+
+    @Test
+    public void shouldTellMoreWhen1000() {
+
+        assertEquals(cash.remain(2754), 246);
+    }
+
+    @Test
+    public void shouldTellLess() {
+
+        assertEquals(cash.remain(999), 1);
+    }
+
+    @Test
+    public void shouldTellAbove() {
+
+        assertEquals(cash.remain(1001), 999);
+    }
+
+    @Test
+    public void shouldTellIfZero() {
+
+        assertEquals(cash.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldNotTellIfBelowZero() {
+
+        assertEquals(cash.remain(-50),0);
+    }
+
+    @Test
+    public void shouldTellMoreThen1000() {
+
+        assertEquals(cash.remain(1000), 1000);
+    }
+}
