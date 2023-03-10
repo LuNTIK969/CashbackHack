80764dec459654ba5e8401c98699794da89d3607
LuNTIK969 <nasa.969@gmail.com>
2023-03-10 18:12:52 +0300

    Test using API JUnit Jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.service;
 
 import org.junit.Test;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.*;
 
@@ -17,37 +19,37 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldTellLess() {
 
-        assertEquals(cash.remain(999), 1);
+        Assertions.assertEquals(cash.remain(999), 1);
     }
 
     @Test
     public void shouldTellAbove() {
 
-        assertEquals(cash.remain(1001), 999);
+        Assert.assertEquals(cash.remain(1001), 999);
     }
 
     @Test
     public void shouldTellIfZero() {
 
-        assertEquals(cash.remain(0), 1000);
+        Assertions.assertEquals(cash.remain(0), 1000);
     }
 
     @Test
     public void shouldNotTellIfBelowZero() {
 
-        assertEquals(cash.remain(-50), 0);
+        Assert.assertEquals(cash.remain(-50), 0);
     }
 
     @Test
     public void shouldNotTellMoreWhen1T() {
 
-        assertEquals(cash.remain(1000), 0);
+        Assertions.assertEquals(cash.remain(1000), 0);
     }
 
     @Test
     public void shouldNotTellMoreWhenAnyT() {
 
-        assertEquals(cash.remain(3000), 0);
+        Assert.assertEquals(cash.remain(3000), 0);
     }
 }
 
