06a698d4f5dc5e15f634cbdc7ee5400beaaec96b
LuNTIK969 <nasa.969@gmail.com>
2023-03-10 17:53:44 +0300

    JUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,53 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService cash = new CashbackHackService();
+
+    @Test
+    public void shouldTellMoreWhen1000() {
+
+        assertEquals(cash.remain(2754), 246);
+    }
+
+    @Test
+    public void shouldTellLess() {
+
+        assertEquals(cash.remain(999), 1);
+    }
+
+    @Test
+    public void shouldTellAbove() {
+
+        assertEquals(cash.remain(1001), 999);
+    }
+
+    @Test
+    public void shouldTellIfZero() {
+
+        assertEquals(cash.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldNotTellIfBelowZero() {
+
+        assertEquals(cash.remain(-50), 0);
+    }
+
+    @Test
+    public void shouldNotTellMoreWhen1T() {
+
+        assertEquals(cash.remain(1000), 0);
+    }
+
+    @Test
+    public void shouldNotTellMoreWhenAnyT() {
+
+        assertEquals(cash.remain(3000), 0);
+    }
+}
+
